1231441026e6cd5b4a53185f856a20f2cbc6d5bc
Felix Seemann <fseemann@mail.de>
2019-11-29 20:42:29 +0100

    Add a stack component

---
+++ src/components/atoms/a-stack.js
@@ -0,0 +1,32 @@
+import React, {useMemo} from 'react'
+import PropTypes from 'prop-types'
+import './a-stack.css'
+import { className } from '../utilities'
+
+export default function AStack (props) {
+  const className = useClassName(props)
+
+  return (
+    <div className={className}>
+      { props.children }
+    </div>
+  )
+}
+
+AStack.propTypes = {
+  spacing: PropTypes.oneOf(['small', 'medium', 'large', 'none']).isRequired
+}
+
+AStack.defaultProps = {
+  spacing: 'medium'
+}
+
+function useClassName (props) {
+  return useMemo(
+    () => className([
+      'a-stack',
+      `a-stack--is-${props.spacing}`
+    ]),
+    [props.spacing]
+  )
+}
--- src/components/templates/t-components.js
+++ src/components/templates/t-components.js
@@ -4,38 +4,39 @@ import React from 'react'
 import './t-components.css'
 import ALabel from '../atoms/a-label'
 import AHeading from '../atoms/a-heading'
+import AStack from '../atoms/a-stack'
 
 export default function TComponents () {
   return (
     <div className="t-components">
-      <AHeading level={1} text={'Components'} margin={'large'}/>
-      <section>
-        <AHeading level={2} text={'Atoms'} margin={'medium'}/>
-        <section>
-          <AHeading level={3} text={'Buttons'} margin={'small'}/>
+      <AHeading level={1} text={'Components'} margin={'large'} />
+      <AStack>
+        <AHeading level={2} text={'Atoms'} margin={'medium'} />
+        <AStack spacing={'small'}>
+          <AHeading level={3} text={'Buttons'} margin={'small'} />
           <AButton />
           <AButton variant={'primary'} />
-        </section>
-        <section>
-          <AHeading level={3} text={'Inputs'} margin={'small'}/>
+        </AStack>
+        <AStack spacing={'small'}>
+          <AHeading level={3} text={'Inputs'} margin={'small'} />
           <AInput type={'text'} placeholder={'Text'} name={'username'} />
           <AInput type={'number'} placeholder={'Number'} name={'username'} />
           <AInput type={'checkbox'} name={'username'} />
-        </section>
-        <section>
-          <AHeading level={3} text={'Labels'} margin={'small'}/>
+        </AStack>
+        <AStack spacing={'small'}>
+          <AHeading level={3} text={'Labels'} margin={'small'} />
           <ALabel for={'some-input-id'} />
-        </section>
-        <section>
-          <AHeading level={3} text={'Headings'} margin={'small'}/>
+        </AStack>
+        <AStack spacing={'small'}>
+          <AHeading level={3} text={'Headings'} margin={'small'} />
           <AHeading level={1} text={'Heading 1'} />
           <AHeading level={2} text={'Heading 2'} />
           <AHeading level={3} text={'Heading 3'} />
           <AHeading level={4} text={'Heading 4'} />
           <AHeading level={5} text={'Heading 5'} />
           <AHeading level={6} text={'Heading 6'} />
-        </section>
-      </section>
+        </AStack>
+      </AStack>
     </div>
   )
 }

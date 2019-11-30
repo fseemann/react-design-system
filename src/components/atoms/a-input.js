cac1c6236f18f4ca7fa0ed6d2b69031ecd980ab8
Felix Seemann <fseemann@mail.de>
2019-11-30 11:37:23 +0100

    Handle checkbox correctly

--- src/components/atoms/a-input.js
+++ src/components/atoms/a-input.js
@@ -5,11 +5,7 @@ import './a-input.css'
 
 export default function AInput (props) {
   const className = useClassName(props)
-  const [value, setValue] = useState('')
-  const handleValueChange = useCallback(
-    (event) => setValue(event.target.value),
-    []
-  )
+  const inputValue = useInputValue(props)
 
   return (
     <input
@@ -19,8 +15,7 @@ export default function AInput (props) {
       placeholder={props.placeholder}
       type={props.type}
       aria-describedby={props.describedBy}
-      onChange={handleValueChange}
-      value={value}
+      { ...inputValue }
     />
   )
 }
@@ -35,6 +30,29 @@ AInput.propTypes = {
 
 AInput.defaultProps = {
   placeholder: 'Input'
+}
+
+function useInputValue (props) {
+  const checkbox = useMemo(() => props.type === 'checkbox', [props.type])
+
+  const [value, setValue] = useState(
+    checkbox ? false : ''
+  )
+
+  const handleValueChange = useCallback(
+    (event) => checkbox ? setValue(event.target.checked) : setValue(event.target.value),
+    [checkbox]
+  )
+
+  const valueProp = useMemo(
+    () => checkbox ? 'checked' : 'value',
+    [checkbox]
+  )
+
+  return {
+    [valueProp]: value,
+    onChange: handleValueChange
+  }
 }
 
 function useClassName (props) {
